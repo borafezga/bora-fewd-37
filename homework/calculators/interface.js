79c931a12cd23252cc4c102cb748535de09638b0
Bora Fezga <bora@kingstreetlabs.com>
2015-11-18 14:55:46 -0800

    Added calculator interface.js & stylesheet

---
+++ homework/calculators/interface.js
@@ -0,0 +1,63 @@
+var pyA = document.querySelector('.py-a');
+var pyB = document.querySelector('.py-b');
+var pyC = document.querySelector('.py-c');
+
+pyA.addEventListener('keyup', calcPy);
+pyB.addEventListener('keyup', calcPy);
+
+function calcPy(event) {
+  if (pyA.value.length > 0 && pyB.value.length > 0) {
+    pyC.value = calcLongestSide(pyA.value, pyB.value)
+  } else {
+    pyC.value = '';
+  }
+}
+
+
+
+
+
+
+var r = document.querySelector('.circum-r');
+var c = document.querySelector('.circum-c');
+
+r.addEventListener('keyup', calcC);
+
+function calcC(event) {
+  if (r.value.length > 0) {
+    c.value = calcCircumference(r.value)
+  } else {
+    c.value = '';
+  }
+}
+
+
+
+
+
+
+// calls the element <input>
+var tempF = document.querySelector('.temp-f');
+// calls the element <input> for celular temp
+var tempC = document.querySelector('.temp-c');
+
+// Eventlisteners tells us which event we want to listen to, which is keyup
+tempF.addEventListener('keyup', calcFtoC);
+tempC.addEventListener('keyup', calcCtoF);
+
+function calcFtoC(event) {
+  if (tempF.value.length > 0) {
+//whatever the function on the right returns gets saved as tempC value
+    tempC.value = calcFahrenheitToCelcius(tempF.value)
+  } else {
+    tempC.value = '';
+  }
+}
+
+function calcCtoF(event) {
+  if (tempC.value.length > 0) {
+    tempF.value = calcCelciusToFarenheit(tempC.value)
+  } else {
+    tempF.value = '';
+  }
+}
